c3ec4bc5d1d2b5ec85fd1e4c2fae0e86d19ec6bb
Brian <brianbawden001@gmail.comgit config --global user.name Brianrungit config --global user.email brianbawden001@gmail.com>
2021-10-19 15:08:45 -0400

    finished def to_string on board.py

--- nim/game/board.py
+++ nim/game/board.py
@@ -6,6 +6,12 @@ class Board():
     This class will make to game board and the updates as the game progresses. 
 
     '''
+    def __init__(self):
+
+        self._piles = []
+        for i in range(0,random.randint(2,5)):
+            n = random.randint(1,9)
+            self._piles.append(n)
 
     def to_string(self):
         '''
@@ -20,17 +26,41 @@ class Board():
 
         return the board
         '''
-        pass
+        dash = 20
+        final_board = " "
+
+
+        for i in range(dash):
+            final_board += ("_ ")
+
+        final_board +="\n\n"
+
+
+        row_num = range(0, len(piles))
+
+        for n in row_num:
+            final_board += (f"{n}: ")
+            stones_in_row = range(1,random.randint(2, 9))
+            for n in stones_in_row:
+                final_board += ("O ")
+            final_board += "\n"
+
+
+        for i in range(dash):
+            final_board += ("_")
+
+        return final_board
 
     def apply(self, move):
-            '''
-            this def tells what row and how many to remove. move will have two numbers the first is what row and the second is how many "O's" to remove. 
-            
-            '''
-            pass
+        '''
+        this def tells what row and how many to remove. move will have two numbers the first is what row and the second is how many "O's" to remove. 
+        
+        '''
+        pass
 
     def is_empty(self):
 
         '''
         this determens if the board is empty
         '''
+        pass
