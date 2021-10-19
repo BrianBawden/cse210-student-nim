03568c5704b187cf538eb9fea5bfe1ee20efed85
Brian <brianbawden001@gmail.comgit config --global user.name Brianrungit config --global user.email brianbawden001@gmail.com>
2021-10-19 14:28:36 -0400

    Working game board

--- nim/game/tests.py
+++ nim/game/tests.py
@@ -15,24 +15,24 @@ final_board = " "
 dash = 20
 
 for i in range(dash):
-    final_board += ("_", end = " ") + "\n\n"
+    final_board += ("_ ")
 
-# print()
+final_board += "\n\n"
 # print()
 
 row_num = range(0,len(piles))
 
 for n in row_num:
-    final_board +=(f"{n}: ", end = "")
+    final_board +=(f"{n}: ")
     stones_in_row = range(2,piles[n])
     for n in stones_in_row:
-        final_board += ("O ", end = "" ) + "\n"
-    print()
+        final_board += ("O " )
+    final_board += "\n"
 
 
 for i in range(dash):
-    final_board += ("_", end = " ")
+    final_board += ("_ ")
 
-print(final)
+print(final_board)
 
 
