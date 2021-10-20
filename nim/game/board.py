64ff795ea06365bbcf696c31583402020cb36737
Brian <brianbawden001@gmail.comgit config --global user.name Brianrungit config --global user.email brianbawden001@gmail.com>
2021-10-20 13:13:52 -0400

    copied answer :(

--- nim/game/board.py
+++ nim/game/board.py
@@ -1,66 +1,34 @@
 import random
 
-class Board():
+class Board:
+    """This class is where the board is created and manipulated."""
 
-    '''
-    This class will make to game board and the updates as the game progresses. 
-
-    '''
     def __init__(self):
 
         self._piles = []
-        for i in range(0,random.randint(2,5)):
-            n = random.randint(1,9)
-            self._piles.append(n)
-
-    def to_string(self):
-        '''
-        you need 20 dashes at top and bottom
-
-        You need random rows between 2-5 and 1-9 random number of O's in each row.: 
-        0: O O O O O
-        1: O
-        2: O O O O O O
-        3: O O O O
-        4: O O O O O O O O
-
-        return the board
-        '''
-        dash = 20
-        final_board = " "
-
-
-        for i in range(dash):
-            final_board += ("_ ")
-
-        final_board +="\n\n"
-
-
-        row_num = range(0, len(piles))
-
-        for n in row_num:
-            final_board += (f"{n}: ")
-            stones_in_row = range(1,random.randint(2, 9))
-            for n in stones_in_row:
-                final_board += ("O ")
-            final_board += "\n"
-
-
-        for i in range(dash):
-            final_board += ("_")
-
-        return final_board
+        self._prepare()
 
     def apply(self, move):
-        '''
-        this def tells what row and how many to remove. move will have two numbers the first is what row and the second is how many "O's" to remove. 
         
-        '''
-        pass
+        pile = move.get_pile()
+        stones = move.get_stones()
+        self._piles[pile] = max(0, self._piles[pile] - stones)
+
+    def to_string(self):
+
+        text = "\n- - - - - - - - - - - - - - "
+        for pile, stones in enumerate(self._piles):
+            text += (f"\n{pile}: " + "O " * stones)
+        text += "\n- - - - - - - - - - - - - - "
+        return text
+    def _prepare(self):
+
+        piles = random.randint(2, 5)
+        for n in range (piles):
+            stones = random.randint(1,9)
+            self._piles.append(stones)
 
     def is_empty(self):
 
-        '''
-        this determens if the board is empty
-        '''
-        pass
+        empty = [0] * len(self._piles)
+        return self._piles == empty
